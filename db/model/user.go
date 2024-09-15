9f2b5321657eee3a5e8069c809264ea890b04cc7
derickwu <xionghengheng@gmail.com>
2024-09-15 10:01:34 +0800

    123
    sdchogn

--- db/model/user.go
+++ db/model/user.go
@@ -12,13 +12,13 @@ type UserInfoModel struct {
 	Age                     int     `json:"age"`                              //年龄
 	Weight                  int     `json:"weight"`                           //体重
 	Height                  int     `json:"height"`                           //身高
-	FitnessExperience       int     `json:"fitness_experience"`               //健身经验
+	FitnessExperience       int     `json:"fitness_experience"`               //健身经验（初级=1，中级=2，高级=3）
 	FitnessGoal             int     `json:"fitness_goal"`                     //健身目标
 	DesiredWeight           int     `json:"desired_weight"`                   //期望体重
 	TimeFrame               int     `json:"time_frame"`                       //期望多快达到
 	PreferredBodyPart       string  `json:"preferred_body_part"`              //最期望增强部位
-	WeeklyExerciseFrequency int     `json:"weekly_exercise_frequency"`        //每周运动次数
-	PreferredPriceRange     int     `json:"preferred_price_range"`            //偏好价格档位
+	WeeklyExerciseFrequency int     `json:"weekly_exercise_frequency"`        //每周运动次数（频次1~2次/周=1，频次3~4次/周=2，频次5~7次/周=3）
+	PreferredPriceRange     int     `json:"preferred_price_range"`            //偏好价格档位(对应的体验课程id)
 	PreferredLocationID     int     `json:"preferred_location_id"`            //偏好健身房场地ID
 	VipType                 int     `json:"vip_type"`                         //vip订阅类型 0=非会员 1=体验会员 2=付费年费会员
 	VipExpiredTs            int64   `json:"vip_expired_ts"`                   //vip过期时间
@@ -28,7 +28,8 @@ type UserInfoModel struct {
 	HeadPicWaitSafe         string  `json:"head_pic_wait_safe"`               //等待审核的头像
 	HeadPicSafeTraceId      string  `json:"head_pic_safe_trace_id"`           //等待审核的traceid，用户和异步回调匹配
 	RegistTs                int64   `json:"regist_ts"`                        //用户注册时间
-	BeVipTs                 int64   `json:"be_vip_ts"`                        //付费成为订阅会员的时间
+	BeVipTs                 int64   `json:"be_vip_ts"`                        //成为订阅会员的时间
+	LastLoginTs             int64   `json:"last_login_ts"`                    //上次登录时间（目前只记录教练的）
 }
 
 const (
