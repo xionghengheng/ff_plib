dc7884498eb2cc58745d0e340740c5a8f6be7d4f
derickwu <xionghengheng@gmail.com>
2024-09-16 10:20:39 +0800

    go123
    123123

--- db/model/coach_client_statistic_cnt.go
+++ db/model/coach_client_statistic_cnt.go
@@ -2,10 +2,10 @@ package model
 
 // 表结构
 type CoachMonthlyStatisticModel struct {
-	CoachID       int    `gorm:"primaryKey" json:"coach_id"`     // 教练ID
-	MonthBegTs    int64  `gorm:"primaryKey" json:"month_beg_ts"` // 统计计数对应的月份
-	PayUserCnt    uint32 `json:"pay_user_cnt"`                   // 付费用户数(去重的)
-	LessonCnt     uint32 `json:"lesson_cnt"`                     // 上课数
-	LessonUserCnt uint32 `json:"lesson_user_cnt"`                // 上课用户数(去重的)
-	SaleRevenue   uint32 `json:"sale_revenue"`                   // 销售额(单位元)
+	CoachID       int    `gorm:"primary_key" json:"coach_id"`     // 教练ID
+	MonthBegTs    int64  `gorm:"primary_key" json:"month_beg_ts"` // 统计计数对应的月份
+	PayUserCnt    uint32 `json:"pay_user_cnt"`                    // 付费用户数(去重的)
+	LessonCnt     uint32 `json:"lesson_cnt"`                      // 上课数
+	LessonUserCnt uint32 `json:"lesson_user_cnt"`                 // 上课用户数(去重的)
+	SaleRevenue   uint32 `json:"sale_revenue"`                    // 销售额(单位元)
 }
