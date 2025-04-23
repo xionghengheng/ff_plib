f9db8b7ceafbe8c4762ecbdc89793517d303482f
derickwu <derickwu@tencent.com>
2025-04-23 09:41:02 +0800

    注释
    +1

--- db/model/course_package.go
+++ db/model/course_package.go
@@ -21,7 +21,7 @@ type CoursePackageModel struct {
 	RefundTs           int64  `json:"refund_ts"`             // 发生退款的时间
 	RefundLessonCnt    int    `json:"refund_lesson_cnt"`     // 退款课程数
 	SendMsgTrailExpire bool   `json:"send_msg_trail_expire"` // 是否已发送消息提醒用户体验课快过期
-	FirstTrialCoachId  int    `json:"first_trial_coach_id"`  // 第一次生成体验课包时候的教练id
+	FirstTrialCoachId  int    `json:"first_trial_coach_id"`  // 第一次生成体验课包时候的教练id (分词要以下划线隔开。。。Error 1054: Unknown column 'first_trial_coach_id' in 'field list')
 }
 
 const (
