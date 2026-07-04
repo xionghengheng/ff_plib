79dddd08c77e7d52132e779976c4025f5d817a90
derickwu <derickwu@tencent.com>
2026-07-04 21:07:47 +0800

    tiasd

--- db/model/task.go
+++ db/model/task.go
@@ -7,7 +7,7 @@ type TaskModel struct {
 	TaskSubName     string `json:"task_sub_name"`    // 任务副标题名称（任务描述）
 	TaskLevel       string `json:"task_level"`       // 任务等级，P0-P3
 	TaskStatus      int    `json:"task_status"`      // 任务状态，0=下架 1=上架
-	DocumentContent string `json:"document_content"` // 文档内容
+	DocumentContent string `json:"document_content"` // 任务具体内容
 	TaskType        int    `json:"task_type"`        // 任务类型，1=纯文本浏览 2=确认式
 	BrowseDuration  int    `json:"browse_duration"`  // 浏览时长，单位秒
 }
@@ -28,3 +28,8 @@ const (
 	Enum_Task_Level_P2 = "P2"
 	Enum_Task_Level_P3 = "P3"
 )
+
+// ConfirmTaskDocumentContent 确认式任务写入DocumentContent的结构化内容
+type ConfirmTaskDocumentContent struct {
+	ContentList []string `json:"content_list"`
+}
